70b700d90a6e4fe5c978885b25ba776e37204284
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-30 10:42:52 -0400

    Interim checkin

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers
+    .filter(driver => driver.hometown === location)
+    .forEach(function (driver) {
+      console.log(driver.name);
+    });
+}
+function driversByRevenue(drivers) {
+  const sortedDrivers = [...drivers];
+  sortedDrivers.sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+  return sortedDrivers;
+}
+
+function driversByName(drivers) {
+  const newarray = [...drivers];
+ return  newarray.sort();
+  
+}
